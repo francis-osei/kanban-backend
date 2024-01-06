3dfb39895a06de6a66cc5b7e5b4f439794ece5cf
cwasi <oseifrancis673@gmail.com>
2024-01-06 01:04:11 +0000

    feat: add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+export interface UserInput {
+    photo: string;
+    fullName: string;
+    email: string;
+    password: string;
+    confirmPassword: string | undefined;
+    passwordChangedAt?: Date | number;
+    passwordResetToken?: string | null;
+    passwordResetExpires: Date;
+    createdAt: Date;
+}
+
+const userSchema = new mongoose.Schema<UserInput>({
+    photo: {
+        type: String,
+        required: [true, 'Please upload your photo'],
+        default: 'default.jpg',
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Email must not be empty'],
+        trim: true,
+        unique: true,
+        lowerCase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+
+    fullName: {
+        type: String,
+        required: [true, 'Please tell use your full name'],
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: [8, 'Password should be more than 8 character'],
+        select: false,
+    },
+
+    confirmPassword: {
+        type: String,
+        required: [8, 'Please confirm your password'],
+        validate: {
+            validator: function (el: string): boolean {
+                const user = this as UserInput;
+                return el === user.password;
+            },
+            message: 'Passwords are not the same',
+        },
+    },
+
+    createdAt: Date,
+});
+
+const User = mongoose.model<UserInput>('User', userSchema);
+
+export default User;
