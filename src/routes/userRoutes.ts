94795af8d8ab3f52ff15224d40905d1e664f6fc6
cwasi <oseifrancis673@gmail.com>
2024-01-18 16:30:25 +0000

    feat: set update user route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import  express  from "express";
-import { createNewUser, deleteUser } from "../controllers/userController";
+import { createNewUser, deleteUser, renewUser } from "../controllers/userController";
 
 const router = express.Router()
 
 
 router.post('/new', createNewUser)
 router.delete('/:id', deleteUser)
+router.patch('/:id', renewUser)
 
 export default router 
