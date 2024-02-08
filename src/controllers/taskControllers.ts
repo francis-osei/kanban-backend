472bd9978713bde65bc94485702211b0f22653d7
cwasi <oseifrancis673@gmail.com>
2024-02-08 14:32:15 +0000

    feat: implement remove task endpoint

--- src/controllers/taskControllers.ts
+++ src/controllers/taskControllers.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import catchAsync from '../utils/catchAsync';
 import { SuccessCodes } from '../utils/statusCode';
 import { findUsersIn } from '../services/userServices';
-import { createTask } from '../services/taskServices';
+import { createTask, deleteTask } from '../services/taskServices';
 import { formatDate } from '../utils/helpers';
+import AppError from '../utils/appError';
 
 export const addTask = catchAsync(async (req: Request, res: Response) => {
     const { assignees, deadline } = req.body;
@@ -24,3 +25,20 @@ export const addTask = catchAsync(async (req: Request, res: Response) => {
         data: { task },
     });
 });
+
+export const removeTask = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+
+        const response = await deleteTask(id);
+
+        if (response instanceof AppError) {
+            return next(new AppError(response.message, response.statusCode));
+        }
+
+        res.status(SuccessCodes.noContent).json({
+            status: 'success',
+            message: 'Task was deleted successfully',
+        });
+    }
+);
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express';
-import { addTask } from '../controllers/taskControllers';
+import { addTask, removeTask } from '../controllers/taskControllers';
 
 const router: Router = express.Router();
 
 router.post('/new', addTask);
+router.delete('/:id', removeTask);
 
 export default router;
